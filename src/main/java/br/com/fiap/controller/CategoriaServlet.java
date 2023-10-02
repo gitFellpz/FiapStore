db7ddb0c50b90226cc840a63844df5d71a071110
gitFellpz <felipe.adrilima@gmail.com>
2023-10-02 11:04:12 -0300

    CategoriaServlet criado com todos os métodos reescritos necessários para operações

---
+++ src/main/java/br/com/fiap/controller/CategoriaServlet.java
@@ -0,0 +1,158 @@
+package br.com.fiap.controller;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.fiap.dao.CategoriaDAO;
+import br.com.fiap.dao.ProdutoDAO;
+import br.com.fiap.exception.DBException;
+import br.com.fiap.factory.DAOFactory;
+import br.com.fiap.model.Categoria;
+import br.com.fiap.model.Produto;
+
+@WebServlet("/categoria")
+public class CategoriaServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private CategoriaDAO dao;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		dao = DAOFactory.getCategoriaDAO();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String acao = request.getParameter("acao");
+
+		switch (acao) {
+		case "cadastrar":
+			cadastrar(request, response);
+			break;
+		case "editar":
+			editar(request, response);
+			break;
+		case "excluir":
+			excluir(request, response);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String acao = request.getParameter("acao");
+
+		switch (acao) {
+		case "listar":
+			listar(request, response);
+			break;
+		case "abrir-form-cadastro":
+			abrirFormCadastro(request, response);
+			break;
+		case "abrir-form-edicao":
+			abrirFormEdicao(request, response);
+			break;
+		}
+	}
+
+	private void abrirFormCadastro(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		carregarOpcoesCategoria(request);
+		request.getRequestDispatcher("cadastro-categoria.jsp").forward(request, response);
+	}
+
+	private void carregarOpcoesCategoria(HttpServletRequest request) {
+		List<Categoria> lista = dao.listar();
+		request.setAttribute("categorias", lista);
+	}
+
+	private void abrirFormEdicao(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("codigo"));
+		Categoria categoria = dao.buscar(id);
+		request.setAttribute("categoria", categoria);
+		carregarOpcoesCategoria(request);
+		request.getRequestDispatcher("edicao-categoria.jsp").forward(request, response);
+	}
+
+	private void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Categoria> lista = dao.listar();
+		request.setAttribute("categoias", lista);
+		request.getRequestDispatcher("lista-categoria.jsp").forward(request, response);
+	}
+
+	private void cadastrar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String nome = request.getParameter("nome");
+
+			Categoria categoria = new Categoria();
+			categoria.setNome(nome);
+
+			dao.cadastrar(categoria);
+
+			request.setAttribute("msg", "Produto cadastrado!");
+		} 
+		catch (DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao cadastrar");
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("erro", "Por favor, valide os dados");
+		}
+		
+		abrirFormCadastro(request, response);
+	}
+
+	private void editar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			int codigo = Integer.parseInt(request.getParameter("codigo"));
+			String nome = request.getParameter("nome");
+
+			Categoria categoria = new Categoria();
+			categoria.setNome(nome);
+
+			dao.atualizar(categoria);
+
+			request.setAttribute("msg", "Produto atualizado!");
+		} 
+		catch (DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao atualizar");
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("erro", "Por favor, valide os dados");
+		}
+		
+		listar(request, response);
+	}
+
+	private void excluir(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int codigo = Integer.parseInt(request.getParameter("codigo"));
+		try {
+			dao.remover(codigo);
+			request.setAttribute("msg", "Produto removido!");
+		} catch (DBException e) {
+			e.printStackTrace();
+			request.setAttribute("erro", "Erro ao atualizar");
+		}
+		listar(request, response);
+	}
+
+}
