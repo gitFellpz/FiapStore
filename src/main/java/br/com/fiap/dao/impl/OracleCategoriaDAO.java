1c717f5c0d99baeb1709eec18b461c4e0427d358
gitFellpz <felipe.adrilima@gmail.com>
2023-10-02 10:49:42 -0300

    método listar em CategoriaDAO reescrito

--- src/main/java/br/com/fiap/dao/impl/OracleCategoriaDAO.java
+++ src/main/java/br/com/fiap/dao/impl/OracleCategoriaDAO.java
@@ -135,47 +135,39 @@ public class OracleCategoriaDAO implements CategoriaDAO {
 		return categoria;
 	}
 
-	@Override
-	public List<Produto> listar() {
-		List<Produto> lista = new ArrayList<Produto>();
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
-			conexao = ConnectionManager.getInstance().getConnection();
-			stmt = conexao.prepareStatement(
-					"SELECT * FROM TB_PRODUTOS INNER JOIN TB_CATEGORIAS ON TB_PRODUTOS.ID_CATEGORIA = TB_CATEGORIAS.ID_CATEGORIA");
-			rs = stmt.executeQuery();
-
-			while (rs.next()) {
-				int codigo = rs.getInt("ID_PRODUTO");
-				String nome = rs.getString("NOME_PRODUTO");
-				int qtd = rs.getInt("QUANTIDADE");
-				double valor = rs.getDouble("VALOR");
-				java.sql.Date data = rs.getDate("DT_FABRICACAO");
-				Calendar dataFabricacao = Calendar.getInstance();
-				dataFabricacao.setTimeInMillis(data.getTime());
-				int codigoCategoria = rs.getInt("ID_CATEGORIA");
-				String nomeCategoria = rs.getString("NOME_CATEGORIA");
-
-				Produto produto = new Produto(nome, valor, dataFabricacao, qtd);
-				Categoria categoria = new Categoria(codigoCategoria, nomeCategoria);
-				produto.setCategoria(categoria);
-				produto.setCodigo(codigo);
-				lista.add(produto);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				stmt.close();
-				rs.close();
-				conexao.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return lista;
-	}
+//	@Override
+//	public List<Categoria> listar() {
+//		List<Categoria> lista = new ArrayList<Categoria>();
+//		PreparedStatement stmt = null;
+//		ResultSet rs = null;
+//		try {
+//			conexao = ConnectionManager.getInstance().getConnection();
+//			stmt = conexao.prepareStatement(
+//					"SELECT * FROM TB_CATEGORIAS");
+//			rs = stmt.executeQuery();
+//
+//			while (rs.next()) {
+//				int codigo = rs.getInt("ID_PRODUTO");
+//				String nome = rs.getString("NOME_PRODUTO");
+//
+//				Categoria categoria = new Categoria(codigo, nome);
+//				lista.add(categoria);
+//			}
+//		} 
+//		catch (SQLException e) {
+//			e.printStackTrace();
+//		} 
+//		finally {
+//			try {
+//				stmt.close();
+//				rs.close();
+//				conexao.close();
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return lista;
+//	}
 
 	@Override
 	public List<Categoria> listar() {
